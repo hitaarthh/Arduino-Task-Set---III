855e8e30da955e67622930d2ebd787ebbc8aff13
hitaarthh <hitarth.rohra@gmail.com>
2022-05-08 01:59:45 +0530

    added source code

--- random.cpp
+++ random.cpp
@@ -1,33 +1,3 @@
-<div>
-<a href="https://www.arduino.cc/"><img src="https://img.shields.io/badge/MicroController%3A-Arduino%20UNO%203-green[700]"height="25" align="left"></a>
-<a href="https://www.tinkercad.com/things/077OPp6fBwy-password-door-lock-system-using-4x4-keypad/editel"><img src="https://img.shields.io/badge/Simulation:-Click%20to%20Simulate -blue" height="25"></a>
-<a href="https://www.microchip.com/en-us/product/ATmega328P"><img src="https://img.shields.io/badge/Processor%3A-Atmega328P-black" height="25" align="right"></a>
-</div>
-
-<div align="center">
-   <h1>Password Door Lock System</h1>
-</div>
-
-### ---> Introduction:
-- A security system now is not only a facility but a necessity. It helps us store data securely and according to our requirements. There are many methods of security and usually, for digital devices, it starts with a password. 
-
-- This circuit is deigned in such a way that it displays a meaningful message on the LCD Screen.
-
-### --> Device Used:
-- Keypad (4x4)
-- Servo Motor
-- LCD Screen(16 x 2)
-- Arduino UNO 3
-- Breadboard
-- Connecting Wires
-
-<a href="https://www.tinkercad.com/things/077OPp6fBwy-password-door-lock-system-using-4x4-keypad/editel"><img src="https://img.shields.io/badge/Simulation:-Click%20to%20Simulate -blue" height="25" ></a>
-
-
-https://user-images.githubusercontent.com/91147942/167269907-9dc3d37d-b846-4e3a-869d-770f3d4c031e.mov
-
-
-```c++
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 #include <Servo.h>
@@ -169,8 +139,3 @@ void Open()
         clearData();
     }
 } 
-```
-
-
-
-
